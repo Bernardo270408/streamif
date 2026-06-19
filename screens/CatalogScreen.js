52930fbf9a0f06d95b4d5df5a0d42903fc92ac15
Beatriz <beatrizcristinacosta33@gmail.com>
2026-06-19 12:49:25 -0300

    Update CatalogScreen.js

--- screens/CatalogScreen.js
+++ screens/CatalogScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, FlatList, Button } from 'react-native';
+import { View, FlatList, Button, Text } from 'react-native';
 import MediaCard from '../components/MediaCard';
 import EmptyState from '../components/EmptyState';
 
 export default function CatalogScreen({ midias, aoAlternarAssistido, aoRemover, aoAbrirDetalhes, abrirModal }) {
+  const totalTitulos = midias.length;
+
+  const totalAssistidos = midias.filter(
+    (midia) => midia.assistido
+  ).length;
+  
   return (
     <View>
       <Button title="+ Adicionar" onPress={abrirModal} />
+      <Text>
+        {totalTitulos} títulos | {totalAssistidos} assistidos
+      </Text>
       
       <FlatList
         data={midias}
