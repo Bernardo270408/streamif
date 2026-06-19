673573eaf3044ecb10c873f277b8e53cdb2ced18
Beatriz <beatrizcristinacosta33@gmail.com>
2026-06-19 13:52:42 -0300

    Update EmptyState.js

--- components/EmptyState.js
+++ components/EmptyState.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function EmptyState() {
   return (
-    <View>
-      <Text>Nenhuma mídia cadastrada no catálogo.</Text>
+    <View style={styles.container}>
+      <Text style={styles.texto}>
+        Nenhuma mídia cadastrada no catálogo.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  texto: {},
+});
